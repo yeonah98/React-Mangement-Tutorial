da7fc393c12f381a4288ef5a4462bbaba257eb2e
yeonah98 <dbdusdkaa@naver.com>
2022-01-03 00:40:37 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React, {useState, useEffect} from 'react';
 import Customer from './component/Customer.js';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material//TableBody';
+import TableRow from '@mui/material//TableRow';
+import TableCell from '@mui/material//TableCell';
+import { withStyles } from '@mui/styles';
+import { createSpacing } from '@mui/system';
 
-function App() {
+const styles =({
+  root: {
+    width: '100%',
+    marginTop: createSpacing.unit*3,
+    oveflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+function App(props) {
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer 
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-            />
-          );
-        })
-      }
-    </div>
+    <Paper className={props.classes.root}>
+      <Table className={props.classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
@@ -50,4 +71,4 @@ const customers = [
  }
 ]
 
-export default App;
+export default withStyles(styles)(App);
--- src/component/Customer.js
+++ src/component/Customer.js
@@ -1,33 +1,18 @@
 import React, {useState, useEffect} from 'react';
+import TableRow from '@mui/material//TableRow';
+import TableCell from '@mui/material//TableCell';
 
 function Customer(props) {
 
     return(
-        <div>
-            <CustomerProfile image={props.image} name={props.name} id={props.id}/>
-            <CustomerInfo birthday={props.birthday} gender={props.gender} job={props.job}/>
-        </div>
-    );
-}
-
-function CustomerProfile(props) {
-
-    return(
-        <div>
-            <img src={props.image} alt="profile"/>
-            <h2>{props.name}({props.id})</h2>
-        </div>
-    );
-}
-
-function CustomerInfo(props) {
-
-    return(
-        <div>
-            <p>{props.birthday}</p>
-            <p>{props.gender}</p>
-            <p>{props.job}</p>
-        </div>
+        <TableRow>
+            <TableCell>{props.id}</TableCell>
+            <TableCell><img src={props.image} alt="profile"/></TableCell>
+            <TableCell>{props.name}</TableCell>
+            <TableCell>{props.birthday}</TableCell>
+            <TableCell>{props.gender}</TableCell>
+            <TableCell>{props.job}</TableCell>
+        </TableRow>
     );
 }
 
